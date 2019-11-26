41887bdd628f46f2d2b905e3663b9ce576b1e5d3
jyhsum <faye.hsu@writepath.co>
2019-11-26 23:50:07 +0800

    update deploy script

--- travis/deploy_script.sh
+++ travis/deploy_script.sh
@@ -4,13 +4,13 @@ create_test_ECS_Service(){
     echo "Create ECS service for functional test"
     aws ecs create-service \
       --cluster demo \
-      --service-name service-for-demo \
+      --service-name demo-service-test \
       --task-definition demo \
       --desired-count 1 \
       --launch-type FARGATE \
       --platform-version LATEST \
-      --network-configuration '{"awsvpcConfiguration":{"subnets":["subnet-0be34124c9a3defec"],"securityGroups":["sg-0c86e8e017fbe6d20"],"assignPublicIp":"ENABLED"}}' \
-      --load-balancers '{"targetGroupArn":"arn:aws:elasticloadbalancing:ap-northeast-1:889231634371:targetgroup/ecs-functional-test-service/d9452ac29980622c","containerName":"dev-container","containerPort":8080}' \
+      --network-configuration '{"awsvpcConfiguration":{"subnets":["subnet-a510e78e"],"securityGroups":["sg-0f73c052e6a8549f7"],"assignPublicIp":"ENABLED"}}' \
+      --load-balancers '{"targetGroupArn":"arn:aws:elasticloadbalancing:ap-northeast-1:294437145362:targetgroup/ecs-demo-service/82131714021d4d77","containerName":"demo-container","containerPort":8000}' \
       --health-check-grace-period-seconds 60 \
       --region ap-northeast-1
 }
@@ -18,16 +18,16 @@ create_test_ECS_Service(){
 create_dev_ECS_Service(){
     echo "Create ECS service for development"
     aws ecs create-service \
-      --cluster writepath-dev \
-      --service-name service-dev \
-      --task-definition writepath-dev \
+      --cluster demo \
+      --service-name demo-service-dev \
+      --task-definition demo \
       --desired-count 1 \
       --launch-type FARGATE \
       --platform-version LATEST \
-      --network-configuration '{"awsvpcConfiguration":{"subnets":["subnet-0be34124c9a3defec"],"securityGroups":["sg-0c86e8e017fbe6d20"],"assignPublicIp":"ENABLED"}}' \
-      --load-balancers '{"targetGroupArn":"arn:aws:elasticloadbalancing:ap-northeast-1:889231634371:targetgroup/ecs-writep-service-dev/39c63b9d1d62d0bf","containerName":"dev-container","containerPort":8080}' \
+      --network-configuration '{"awsvpcConfiguration":{"subnets":["subnet-a510e78e"],"securityGroups":["sg-0f73c052e6a8549f7"],"assignPublicIp":"ENABLED"}}' \
+      --load-balancers '{"targetGroupArn":"arn:aws:elasticloadbalancing:ap-northeast-1:294437145362:targetgroup/ecs-demo-service/82131714021d4d77","containerName":"demo-container","containerPort":8000}' \
       --health-check-grace-period-seconds 60 \
-      --region ap-northeast-1;
+      --region ap-northeast-1
 }
 
 "$@"
